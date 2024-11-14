65be07bd1b788c464fc6e26c6d35dc7c0cbe4041
Felix Samsonov <german1samsonov@gmail.com>
2024-11-14 23:13:14 +0200

    add my first home work

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int n1 = 1;
+string client1 = "Alice";
+string product1 = "smartphone";
+double price1 = 305.99;
+string address1 = "Moon street";
+string number1 = "10";//присвоїв тип даних "string" тому що адреса може мати "/"
+Console.WriteLine($"Order No {n1}, \nClient: {client1}, \nProduct: {product1}, price: {price1} EUR. \nAddress: {address1}, {number1}.\n");
+
+int n2 = 2;
+string client2 = "Tom";
+string product2 = "laptop";
+double price2 = 570.95;
+string address2 = "Terra street";
+string number2 = "17";
+Console.WriteLine($"Order No {n2}, \nClient: {client2}, \nProduct: {product2}, price: {price2} EUR. \nAddress: {address2}, {number2}.\n");
+
+int n3 = 3;
+string client3 = "Jack";
+string product3 = "keyboard";
+double price3 = 5.50;
+string address3 = "Green Wally";
+string number3 = "32";
+Console.WriteLine($"Order No {n3}, \nClient: {client3}, \nProduct: {product3}, price: {price3} EUR. \nAddress: {address3}, {number3}.");
